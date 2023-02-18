d8d8564918582cb32a6ef4176a871e3cb5b3769a
tramanhvong <vongta21@my.yorku.ca>
2023-02-18 14:15:01 -0500

    redirect

--- floraClient.js
+++ floraClient.js
@@ -1,24 +1,8 @@
 
 var url = "http://localhost:3000/post";
 
-
-function start() {
-        /*var newSpan=document.createElement("span");
-        $(newSpan).attr("id","locationSpan"); //set attribute by this
-        var form=document.createElement("form");
-        $(form).attr("label","INPUT YOUR LOCATION");
-        $(form).attr("type","location");
-        $(form).attr("id","location");
-        $(form).attr("name","location");
-        $(newSpan).append(form);*/
-       // window.location.href="infoOutput.html";
-        document.getElementById("tree").className="tree";
-        document.getElementById("start").style.display="none";
-            
-}
-
 function load() {
-    var loc = document.getElementById("region").value;
+    var loc = document.getElementById("location").value;
     $.post(
         url+'?data='+JSON.stringify
         ({ //compare the input name with the database
