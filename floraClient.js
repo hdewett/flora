7848ac63377fa7cee4ebdd83fcb951696154a3dd
tramanhvong <vongta21@my.yorku.ca>
2023-02-18 09:21:30 -0500

    html redirect instead haish

--- floraClient.js
+++ floraClient.js
@@ -3,22 +3,19 @@ var url = "http://localhost:3000/post";
 
 
 function start() {
-        var newSpan=document.createElement("span");
+        /*var newSpan=document.createElement("span");
         $(newSpan).attr("id","locationSpan"); //set attribute by this
         var form=document.createElement("form");
         $(form).attr("label","INPUT YOUR LOCATION");
         $(form).attr("type","location");
         $(form).attr("id","location");
         $(form).attr("name","location");
-        $(newSpan).append(form);
+        $(newSpan).append(form);*/
+        window.location.href="infoOutput.html";
         
         $.post(
             url+'?data='+JSON.stringify
             ({ //compare the input name with the database
-            'name':name,
-            'major': major,
-            'day':day,
-            'action':'createProfile'
             }),
             response);
             
