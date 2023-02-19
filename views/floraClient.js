19ef1c0b68c243a3191794911cf19a53f7848ea2
tramanhvong <vongta21@my.yorku.ca>
2023-02-19 02:41:11 -0500

    deleted comments and changed names for classes

--- views/floraClient.js
+++ views/floraClient.js
@@ -32,35 +32,18 @@ function parse(s) {
         response);
 }
 
-function turnArray(s) {
-    var out=["start"];
-    while (s.length>0) {
-        var i = s.indexOf("<br/>");
-        s.substring(i+1,s.length);
-        i = s.indexOf("<br/>");
-        var line = s.substring(0,i);
-        out.push(line);
-    }
-
-}
-
 function response(data,status) {
     var response = JSON.parse(data);
     console.log(data);
     if (response['action'] == 'infoLoaded') {
-        var resp = response['result']; 
-        alert(resp);
-        document.getElementById("plantOne").setAttribute("id","WHITE OAK");
-        /*var p1 = resp[0];var p2=resp[1];var p3=resp[2];
-        alert(p1);
-        document.getElementById("plantOne").setAttribute(resp[0]);
-        document.getElementById("plantTwo").setAttribute(resp[1]);
-        document.getElementById("plantThree").setAttribute(resp[2]);*/
+        var resp = response['result'];
+        resp = resp.replace(/[0-9]|-|\.|The native plants are| the |The three native plants are/gi,'');
+        resp = resp.replace(/,| and |\n/gi,'|');
+        document.getElementById("test").innerHTML=resp;
+        const arr=resp.split("|");
+        document.getElementById("plant1").innerHTML=arr[1];
+        document.getElementById("plant2").innerHTML=arr[2];
+        document.getElementById("plant3").innerHTML=arr[3];
     }
-    /*else if (response['action']=='stringParsed') {
-        alert("step2Check");
-        var resp2 = response['result'];
-        document.getElementById("output").innerHTML=resp2;
-    }*/
     
 }
